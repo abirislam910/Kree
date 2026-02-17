1b8bdf80765f1fcf5fda694b7ce91e911d1f7244
Abir Islam <abirislam910@gmail.com>
2026-02-16 21:29:33 -0500

    Reconfigured app to use react-router-dom, began Supabase integration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Registration from "./Pages/Registration";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
