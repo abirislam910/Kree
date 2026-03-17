4eb72082182bdb515b3ee61ee781f847d141045b
Abir Islam <abirislam910@gmail.com>
2026-03-17 18:15:14 -0400

    Refactored generation results to their own route

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(cors({
     res.set('Access-Control-Allow-Credentials', 'true');
     res.status(200).send();
 });
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 console.log(ORIGIN);
 
